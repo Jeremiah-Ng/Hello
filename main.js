edde7cbadfb51af1765982ae07d42a6c0c0d9272
Jeremiah-Ng <72901442+Jeremiah-Ng@users.noreply.github.com>
2020-10-17 14:40:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function change(){
+document.getElementById("image1").src="https://www.kew.org/sites/default/files/2020-04/Water%20droplets%20on%20green%20leaf.jpg"
+}
+function change1(){
+    document.getElementById("image2").src="https://media.istockphoto.com/photos/tree-growth-three-steps-in-nature-and-beautiful-morning-lighting-picture-id956366756?k=6&m=956366756&s=612x612&w=0&h=sqi3kTfxi02Y-6xyyGmOjp9LI5ES_gipovhXenzaPPo="
+}
+function change2(){
+    document.getElementById("image3").src="https://e2k9ube.cloudimg.io/s/cdn/x/https://edienet.s3.amazonaws.com/news/images/full_40419.jpg?v=15/06/2020%2016:52:00"
+}
+function change3(){
+    document.getElementById("image4").src="https://img.fixthephoto.com/blog/images/gallery/news_preview_mob_image__preview_579.jpg"
+}
